b163b84ae0b144d776bfca68756db05159c45501
Dawin <60050173@kmitl.ac.th>
2018-09-10 18:48:32 +0700

    add : adder.java

---
+++ Adder.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Adder
+{
+    public static void main (String[] args)
+        {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter x = ");
+        int x = sc.nextInt();
+        System.out.print("Enter y = ");
+        int y = sc.nextInt();
+        int z = x + y;
+        System.out.println("Ans = "+z);
+    }
+}
